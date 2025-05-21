e2cad09a34df26f29d8df8d1c052309d6ddb756a
Kliona Kennet <klionakennet2002@gmail.com>
2025-05-21 21:17:15 +1000

    Updated files for week 7

--- Week7/7.2/week7/public/js/script.js
+++ Week7/7.2/week7/public/js/script.js
@@ -1,18 +1,16 @@
 const cardList = [
   {
-      title: "Maybelline",
-      image: "images/maybelline.jpg",
-      link: "Maybe its maybelline",
-      desciption:
-          "Maybelline New York is the number one cosmetic brand in the world, available in over 120 countries. By combining technologically advanced formulations with on-trend expertise and New York edge, Maybelline New York's mission is to offer innovative, accessible and effortless cosmetics for every woman.",
+    title: "Maybelline",
+    image: "images/maybelline.jpg",
+    link: "Maybe it's Maybelline",
+    desciption: "Maybelline is a leading cosmetic brand available in over 120 countries, offering innovative and accessible beauty products."
   },
   {
-      title: "Lakme",
-      image: "images/lakme.jpg",
-      link: "Let your nails do the talking!",
-      desciption:
-          "Lakmé is an Indian cosmetics brand owned by Hindustan Unilever, known for its wide range of cosmetics, beauty products, and salon services, with a focus on professional-grade products and sustainable practices",
-  },
+    title: "Lakme",
+    image: "images/lakme.jpg",
+    link: "Let your nails do the talking!",
+    desciption: "Lakmé is a trusted Indian cosmetics brand known for wide beauty product offerings and professional-grade salon services."
+  }
 ];
 
 const clickMe = () => {
@@ -20,56 +18,67 @@ const clickMe = () => {
 };
 
 const submitForm = () => {
-  let formData = {};
-  formData.first_name = $("#first_name").val();
-  formData.last_name = $("#last_name").val();
-  formData.password = $("#password").val();
-  formData.email = $("#email").val();
+  const formData = {
+    first_name: $("#first_name").val(),
+    last_name: $("#last_name").val(),
+    password: $("#password").val(),
+    email: $("#email").val()
+  };
   console.log("Form Data Submitted: ", formData);
 };
 
 const addCards = (items) => {
-  items.forEach((item) => {
-      let itemToAppend =
-          '<div class="col s4 center-align">' +
-          '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
-          item.image +
-          '" >' +
-          '</div><div class="card-content">' +
-          '<span class="card-title activator grey-text text-darken-4">' +
-          item.title +
-          '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
-          item.link +
-          "</a></p></div>" +
-          '<div class="card-reveal">' +
-          '<span class="card-title grey-text text-darken-4">' +
-          item.title +
-          '<i class="material-icons right">close</i></span>' +
-          '<p class="card-text" style="color: black">' +
-          item.desciption +
-          "</p>" +
-          "</div></div></div>";
-      $("#card-section").append(itemToAppend);
+  const row = $('<div class="row"></div>');
+  items.forEach(item => {
+    const cardHTML = `
+      <div class="col s4 center-align">
+        <div class="card medium">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="${item.image}">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">${item.title}
+              <i class="material-icons right">more_vert</i>
+            </span>
+            <p><a href="#">${item.link}</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">${item.title}
+              <i class="material-icons right">close</i>
+            </span>
+            <p class="card-text" style="color: black">${item.desciption}</p>
+          </div>
+        </div>
+      </div>`;
+    row.append(cardHTML);
   });
+  $("#card-section").append(row);
 };
 
 $(document).ready(function () {
   $(".materialboxed").materialbox();
-  $(".modal").modal(); 
+  $(".modal").modal();
 
-  var socket = io();
+  const socket = io();
+
   socket.on('connect', () => {
-      console.log('Connects to the socket server');
+    console.log('Connected to socket server');
+  });
+
+  socket.on('update', (data) => {
+    console.log('Received update:', data);
+    const log = $('<p></p>').text(data);
+    $('#live-updates').append(log);
   });
 
   $("#clickMeButton").click(() => {
-      $("#modal1").modal("open"); 
+    $("#modal1").modal("open");
   });
 
   $("#formSubmit").click(() => {
     submitForm();
     $("#modal1").modal("close");
-});
+  });
 
   addCards(cardList);
 });
--- Week7/7.2/week7/server.js
+++ Week7/7.2/week7/server.js
@@ -4,15 +4,21 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-  console.log('An user is connected');
-  socket.on('disconnected', () => {
-    console.log('An user is disconnected');
+  console.log('A user is connected');
+
+  const intervalId = setInterval(() => {
+    const message = `Server time: ${new Date().toLocaleTimeString()}`;
+    socket.emit('update', message);
+  }, 5000);
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+    clearInterval(intervalId);
   });
 });
 
-http.listen(port, () =>
-  console.log(`App listening to: http://localhost:${port} `)
-);
+http.listen(port, () => console.log(`App listening to: http://localhost:${port}`));
